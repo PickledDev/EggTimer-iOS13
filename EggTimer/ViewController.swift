2c29bf827469feaf358ee6a45af948785bab79e5
Michael Irwin <33363962+PickledDev@users.noreply.github.com>
2020-11-14 10:37:39 -0600

    Simple Egg Timer
    Learning iOS built an egg timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var progressBar: UIProgressView!
+  
+  let eggTimes = ["Soft":3, "Medium":4, "Hard":7]
+  
+  var timer = Timer()
+  var totalTime = 0
+  var secondsPassed = 0
+  
+  var player : AVAudioPlayer?
+  
+  @IBAction func hardnessSelected(_ sender: UIButton) {
+    timer.invalidate()
+    progressBar.progress = 0.0
+    secondsPassed = 0
+    let hardness = sender.currentTitle!
+    titleLabel.text = hardness
     
+    totalTime = eggTimes[hardness]!
+  
+    timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+  }
+  
+  @objc func updateTimer() {
+    if secondsPassed < totalTime {
+      secondsPassed += 1
+      progressBar.progress = Float(secondsPassed) / Float(totalTime)
 
+    } else {
+      timer.invalidate()
+      
+      guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
 
+      do {
+          try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+          try AVAudioSession.sharedInstance().setActive(true)
+
+          player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+          guard let player = player else { return }
+
+          player.play()
+
+      } catch let error {
+          print(error.localizedDescription)
+      }
+      
+      
+      titleLabel.text = "Done!"
+    }
+  }
 }
